3a251e7f4d534b56029366177d337700956c6b9e
YangXuuu <92423698+YangXuuu@users.noreply.github.com>
2025-06-17 13:49:35 +1000

    Update Run_Create_Obj.R

--- R/Run_Create_Obj.R
+++ R/Run_Create_Obj.R
@@ -13,109 +13,92 @@
 #' @param ss.radius Optional. Radius for filtering SlideSeq or Curio-seeker spots for Seurat object. Default is 3000.
 #' @param tech Type of spatial transcriptomics sequencing technology, can be "Visium", "Slideseq", "Curio-seeker", or "Stereoseq".
 #' @return Created spatial transcriptomics data object as required by 'obj.type'.
-#' @examples
-#' gene.matrix <- matrix(sample(1:100, 10 * 5, replace = TRUE), nrow = 10)
-#' rownames(gene.matrix) <- paste0("Gene", 1:10)
-#' colnames(gene.matrix) <- paste0("Spot", 1:5)
+#' set.seed(123)
+#' gene.count <- matrix(sample(0:100, 200*100, TRUE), nrow=200)
+#' rownames(gene.count) <- paste0('Gene', 1:200)
+#' colnames(gene.count) <- paste0('Spot', 1:100)
 #' matched.data <- data.frame(
-#' cell_name = paste0("Spot", 1:5),
-#' barcode_sequence = paste0("BC", 1:5),
-#' xcoord = runif(5, 0, 100),
-#' ycoord = runif(5, 0, 100),
-#' stringsAsFactors = FALSE)
-#' obj <- Run_Create_Obj(gene.matrix = gene.matrix, 
-#' matched.data = matched.data, 
-#' obj.type = "SpatialExperiment", 
-#' tech = "Visium")
+#'   spatial_name = colnames(gene.count),
+#'   barcode_sequence = paste0('BC', 1:100),
+#'   X_coordinate = runif(100,0,10),
+#'   Y_coordinate = runif(100,0,10),
+#'   UMI_count = sample(50:200,100,TRUE)
+#' )
+#' seu <- Run_Create_Obj(
+#'   gene.matrix = gene.count,
+#'   matched.data = matched.data,
+#'   obj.type = 'Seurat',
+#'   tech = 'Slideseq'
+#' )
 #' @export
 #' @importFrom ggplot2 theme
 
 Run_Create_Obj <- function(gene.matrix, matched.data, obj.type, tech, ss.radius = 3000) {
-
-  if (!all(matched.data$cell_name %in% colnames(gene_count))) {
+  
+  if (!all(matched.data$spatial_name %in% colnames(gene.matrix))) {
     stop("Spatial locations in matched.data do not match column names of gene.matrix!")
   }
-
-  # replace spot names with spatial barcode sequences
-  gene_columns <- colnames(gene.matrix)
-  mapping <- setNames(matched.data$barcode_sequence, matched.data$cell_name)
-  new_columns <- mapping[gene_columns]
-  if (all(!is.na(new_columns))) {
-    colnames(gene.matrix) <- new_columns
-  } else {
-    warning("Some spatial location names did not have a matching barcode sequence.")
-  }
-
-  # get matched meta.data with gene.matrix and reformat
-  matched.data <- obj@meta.data[,4:5]
-  rownames(matched.data) <- matched.data$barcode_sequence
-  matched.data <- matched.data[, 4:5]
-  colnames(matched.data) <- c("xcoord", "ycoord")
-
+  
+  counts_mat <- as.matrix(gene.matrix)
+  matched.data <- matched.data[match(colnames(counts_mat), matched.data$spatial_name), ]
+  meta_df <- data.frame(
+    X   = matched.data$X_coordinate,
+    Y   = matched.data$Y_coordinate,
+    UMI = matched.data$UMI_count,
+    row.names = matched.data$barcode_sequence
+  )
+  colnames(counts_mat) <- matched.data$barcode_sequence
+  
   # Create Seurat object
   if (obj.type == "Seurat") {
-
+    
     # Create a Seurat object
-    obj <- SeuratObject::CreateSeuratObject(counts = gene.matrix, meta.data = matched.data, assay = 'Spatial')
-
-    # add spatial information to created Seurat object based on different technology
-    if (tech == "Visium"){
-
-      matched.data$ycoord <- -matched.data$ycoord
-      assay_name <- "Spatial"
-      fov_name <- "fov"
-      fov <- SeuratObject::CreateFOV(
-        matched.data,
-        type = "centroids",
-        assay = assay_name,
-        key = Key(fov_name, quiet = TRUE))
-      fov <- fov[SeuratObject::Cells(obj)]
-      obj[[fov_name]] <- fov
-
-    } else if (tech == "Slideseq" | tech == "Curio-seeker") {
-
+    obj <- SeuratObject::CreateSeuratObject(counts = counts_mat, meta.data = meta_df, assay = 'Spatial')
+    coords <- meta_df[, c("X","Y"), drop = FALSE]
+    
     obj[['image']] <- new(
       Class = 'SlideSeq',
       assay = "Spatial",
-      coordinates = matched.data
-    )
-    # remove stray beads which fall outside the main Slideseq puck area
-    obj <- Seurat::FilterSlideSeq(object = obj, radius = ss.radius, do.plot = TRUE)
+      coordinates = coords)
+    
+    # add spatial information to created Seurat object based on different technology
+    if (tech == "Slideseq" | tech == "Curio-seeker"){
+      
+      # remove stray beads which fall outside the main Slideseq puck area
+      obj <- Seurat::FilterSlideSeq(object = obj, radius = ss.radius, do.plot = TRUE)
+      
     }
-
-
   } else if (obj.type == "SpatialExperiment") {
-
+    
     obj <- SpatialExperiment::SpatialExperiment(
-      assays = list(counts = gene.matrix),
-      spatialCoords = as.matrix(matched.data)
+      assays = list(counts = counts_mat),
+      colData = meta_df,
+      spatialCoords = as.matrix(meta_df[, c('X','Y')])
     )
-    rowData(obj)$gene_name <- rownames(rowData(obj))
-
-
+    rowData(obj)$gene_name <- rownames(gene.matrix)
+    
+    
   } else if (obj.type == "AnnData") {
-
+    
     # Create an AnnData object
     anndata <- reticulate::import("anndata")
     obj <- anndata$AnnData(X = gene.matrix)
     obj$obs <- matched.data
-
+    
     py_create_anndata <- "
     import anndata
     adata = anndata.AnnData(X = r.gene_matrix)
     adata.obs = r.matched_data
     "
     reticulate::py_run_string(py_create_anndata)
-
+    
     obj <- reticulate::py$adata
-
+    
   } else {
-
-    stop("Unsupported obj.type! Please choose from 'Seurat', 'SpatialExperiment', or 'AnnData'.")
+    
+    stop("Unknown obj.type! Please choose class from 'Seurat', 'SpatialExperiment', or 'AnnData'.")
   }
-
+  
   return(obj)
-
-
-
+  
 }
